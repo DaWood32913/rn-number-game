3f2c39e134658201193899ac6867f1abadcf8030
DaWood32913 <woodleyjohnson@yahoo.com>
2020-02-04 16:27:01 -0500

    configured the restarting of the game

--- App.js
+++ App.js
@@ -1,29 +1,41 @@
-import React, { useState } from 'react';
-import { StyleSheet, View } from 'react-native';
-import Header from './components/Header';
-import StartGameScreen from './screens/StartGameScreen';
-import GameScreen from './screens/GameScreen';
-import GameOverScreen from './screens/GameOverScreen';
+import React, { useState } from "react";
+import { StyleSheet, View } from "react-native";
+import Header from "./components/Header";
+import StartGameScreen from "./screens/StartGameScreen";
+import GameScreen from "./screens/GameScreen";
+import GameOverScreen from "./screens/GameOverScreen";
 
 export default function App() {
   const [userNumber, setUserNumber] = useState();
   const [guessRounds, setGuessRounds] = useState(0);
 
-  const startGameHandler = (selectedNumber) => {
-    setUserNumber(selectedNumber);
+  const configureNewGameHandler = () => {
     setGuessRounds(0);
+  };
+
+  const startGameHandler = selectedNumber => {
+    setUserNumber(selectedNumber);
   };
 
   const gameOverHandler = numOfRounds => {
     setGuessRounds(numOfRounds);
+    setUserNumber(null);
   };
 
   let content = <StartGameScreen onStartGame={startGameHandler} />;
 
-  if(userNumber && guessRounds <= 0) {
-     content = <GameScreen userChoice={userNumber} onGameOver={gameOverHandler} />;
+  if (userNumber && guessRounds <= 0) {
+    content = (
+      <GameScreen userChoice={userNumber} onGameOver={gameOverHandler} />
+    );
   } else if (guessRounds > 0) {
-    content = <GameOverScreen />;
+    content = (
+      <GameOverScreen
+        roundsNumber={guessRounds}
+        userNumber={userNumber}
+        onRestart={configureNewGameHandler}
+      />
+    );
   }
 
   return (
@@ -38,5 +50,4 @@ const styles = StyleSheet.create({
   screen: {
     flex: 1
   }
-
 });
