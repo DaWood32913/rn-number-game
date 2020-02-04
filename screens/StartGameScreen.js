7f623e9881b871ea4d28834a22b405ac77554dbb
DaWood32913 <woodleyjohnson@yahoo.com>
2020-02-03 19:35:21 -0500

    created input component. have to fix borderBottom width

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,7 +1,9 @@
 import React from "react";
-import { View, Text, StyleSheet, TextInput, Button } from "react-native";
+import { View, Text, StyleSheet, Button } from "react-native";
 import Card from "../components/Card";
+import Input from "../components/Input";
 import Colors from "../constants/colors";
+
 
 const StartGameScreen = props => {
   return (
@@ -9,10 +11,14 @@ const StartGameScreen = props => {
       <Text style={styles.title}>Start a New Game!</Text>
       <Card style={styles.inputContainer}>
         <Text>Choose a Number</Text>
-        <TextInput />
+        <Input />
         <View style={styles.buttonContainer}>
-          <View style={styles.button}><Button title="Reset" onPress={() => {}} color={Colors.accent}/></View>
-          <View style={styles.button}><Button title="Confirm" onPress={() => {}} color={Colors.primary} /></View>
+          <View style={styles.button}>
+            <Button title="Reset" onPress={() => {}} color={Colors.accent} />
+          </View>
+          <View style={styles.button}>
+            <Button title="Confirm" onPress={() => {}} color={Colors.primary} />
+          </View>
         </View>
       </Card>
     </View>
@@ -41,7 +47,7 @@ const styles = StyleSheet.create({
     paddingHorizontal: 15
   },
   button: {
-      width: 100
+    width: 100
   }
 });
 
